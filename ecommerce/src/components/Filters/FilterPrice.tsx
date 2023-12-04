725404e31a259ec8ca948ba25c453832d39d1eed
Evelyn Matos <evelynmatos@wine.com.br>
2023-12-04 17:21:52 -0300

    refactor: melhorando os checkbox, deixando dinamico

--- ecommerce/src/components/Filters/FilterPrice.tsx
+++ ecommerce/src/components/Filters/FilterPrice.tsx
@@ -31,25 +31,25 @@ export const FilterPrice = () => {
         dispatch(setPrice(value === filterPreco ? '' : value))
 
         if (value === faixaPreco) {
-            
-            if(KeyWord){
-                
+
+            if (KeyWord) {
+
                 navigate(`/loja/search?pg=1&keyword=${KeyWord}&keyprice=all`);
-            }else{
-                
+            } else {
+
                 navigate(`/loja/search?pg=1&keyword=all&keyprice=all`)
             }
-              
-            
-        }else if(value !== '' ){
-          
-            if(KeyWord != null){
+
+
+        } else if (value !== '') {
+
+            if (KeyWord != null) {
                 navigate(`/loja/search?pg=1&keyword=${KeyWord}&keyprice=${value}`);
-            }else {
+            } else {
                 navigate(`/loja/search?pg=1&keyword=all&keyprice=${value}`);
             }
 
-            
+
 
         }
 
@@ -61,103 +61,43 @@ export const FilterPrice = () => {
     }, [filterPreco])
 
 
+    const faixaPrecoOptions = [
+
+        { value: '0-40', label: 'Até R$40,00' },
+        { value: '40-60', label: 'R$40 a R$60' },
+        { value: '100-200', label: 'R$100 a R$200' },
+        { value: '200-500', label: 'R$200 a R$500' },
+        { value: '500-999999', label: 'Acima de R$500' },
+    ];
+
+
     return (
         <div className=" mt-8  mb-4 md:w-screen lg:w-60">
             <label htmlFor="faixaPreco" className="text-[#333] text-lg font-bold">Por preço</label>
             <div className="select-wrapper mt-4 md:flex  lg:flex-col ">
 
+                {faixaPrecoOptions.map((option, index) => (
+                    <div key={index} className={`option ${faixaPreco.includes(option.value) ? 'selected' : ''} flex items-center md:mr-4 lg:mr-0`}>
+                        <Checkbox
+                            name="faixaPreco"
+                            value={option.value}
+                            id={option.value}
+                            checked={faixaPreco.includes(option.value)}
+                            onChange={handleFaixaPrecoChange}
+                            icon={<RadioButtonUncheckedIcon sx={{ fontSize: 20, color: '#C81A78' }} />} // Ícone quando não está marcado
+                            checkedIcon={<RadioButtonCheckedIcon sx={{ fontSize: 20, color: '#C81A78' }} />} // Ícone quando está marcado
+                            sx={{
+                                paddingLeft: '0px',
+                                
+                            }}
+                        />
 
-                <div className={`option ${faixaPreco.includes('0-40') ? 'selected' : ''} flex items-center md:mr-4 lg:mr-0`}>
-                    <Checkbox
-                        name="faixaPreco"
-                        value="0-40"
-                        id="0-40"
-                        checked={faixaPreco.includes('0-40')}
-                        onChange={handleFaixaPrecoChange}
-                        icon={<RadioButtonUncheckedIcon sx={{ fontSize: 20, color: '#C81A78' }} />} // Ícone quando não está marcado
-                        checkedIcon={<RadioButtonCheckedIcon sx={{ fontSize: 20, color: '#C81A78' }} />} // Ícone quando está marcado
-                        sx={{
-                            paddingLeft: '0px'
-                        }}
-                    />
+                        <label htmlFor={option.value} className="text-[#1D1D1B] text-sm text-center">{option.label}</label>
 
-                    <label htmlFor="até40" className="text-[#1D1D1B] text-sm text-center">Até R$40,00</label>
+                    </div>
 
-                </div>
-
-                <div className={`option ${faixaPreco.includes('40-60') ? 'selected' : ''} flex items-center md:mr-4 lg:mr-0`}>
-                    <Checkbox
-
-                        name="faixaPreco"
-                        value="40-60"
-                        id="40-60"
-                        checked={faixaPreco.includes('40-60')}
-                        onChange={handleFaixaPrecoChange}
-                        icon={<RadioButtonUncheckedIcon sx={{ fontSize: 20, color: '#C81A78' }} />} // Ícone quando não está marcado
-                        checkedIcon={<RadioButtonCheckedIcon sx={{ fontSize: 20, color: '#C81A78' }} />} // Ícone quando está marcado
-                        sx={{
-                            paddingLeft: '0px'
-                        }}
-                    />
-                    <label htmlFor="40a60" className="text-[#1D1D1B] text-sm text-center">R$40 a R$60</label>
-                </div>
-
-
-
-                <div className={`option ${faixaPreco.includes('100-200') ? 'selected' : ''} flex items-center md:mr-4 lg:mr-0`}>
-                    <Checkbox
-                        name="faixaPreco"
-                        value="100-200"
-                        id="100-200"
-                        checked={faixaPreco.includes('100-200')}
-                        onChange={handleFaixaPrecoChange}
-                        icon={<RadioButtonUncheckedIcon sx={{ fontSize: 20, color: '#C81A78' }} />} // Ícone quando não está marcado
-                        checkedIcon={<RadioButtonCheckedIcon sx={{ fontSize: 20, color: '#C81A78' }} />} // Ícone quando está marcado
-                        sx={{
-                            paddingLeft: '0px'
-                        }}
-                    />
-                    <label htmlFor="100a200" className="text-[#1D1D1B] text-sm text-center">R$100 a R$200</label>
-
-                </div>
-
-
-
-                <div className={`option ${faixaPreco.includes('200-500') ? 'selected' : ''} flex items-center md:mr-4 lg:mr-0`}>
-                    <Checkbox
-                        name="faixaPreco"
-                        value="200-500"
-                        id="200-500"
-                        checked={faixaPreco.includes('200-500')}
-                        onChange={handleFaixaPrecoChange}
-                        icon={<RadioButtonUncheckedIcon sx={{ fontSize: 20, color: '#C81A78' }} />} // Ícone quando não está marcado
-                        checkedIcon={<RadioButtonCheckedIcon sx={{ fontSize: 20, color: '#C81A78' }} />} // Ícone quando está marcado
-                        sx={{
-                            paddingLeft: '0px'
-                        }}
-                    />
-                    <label htmlFor="200-500" className="text-[#1D1D1B] text-sm text-center" >R$200 a R$500</label>
-
-                </div>
-
-
-                <div className={`option ${faixaPreco.includes('500-999999') ? 'selected' : ''} flex items-center md:mr-4 lg:mr-0`}>
-                    <Checkbox
-                        name="faixaPreco"
-                        value="500-999999"
-                        id="500-999999"
-                        checked={faixaPreco.includes('500-999999')}
-                        onChange={handleFaixaPrecoChange}
-                        icon={<RadioButtonUncheckedIcon sx={{ fontSize: 20, color: '#C81A78' }} />} // Ícone quando não está marcado
-                        checkedIcon={<RadioButtonCheckedIcon sx={{ fontSize: 20, color: '#C81A78' }} />} // Ícone quando está marcado
-                        sx={{
-                            paddingLeft: '0px'
-                        }}
-                    />
-                    <label htmlFor="500-999999" className="text-[#1D1D1B] text-sm text-center">Acima de R$500</label>
-
-                </div>
-
+                ))}
+  
             </div>
         </div>
     );
